56db88912262f9c3d7fdb7072b3360bf0447d8ee
shekharramnik43-blip <shekhar.ramnik.43@gmail.com>
2026-05-13 23:58:58 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -1,27 +1,32 @@
 const standings = [
+  { driver: "late", team: "Alpine", points: 72 },
   { driver: "xyro", team: "Audi", points: 58 },
-  { driver: "Iate", team: "Alpine", points: 47 },
-  { driver: "Yatneil", team: "Audi", points: 40 },
+  { driver: "Yatneil", team: "Audi", points: 46 },
   { driver: "Vini", team: "Peugeot", points: 26 },
+  { driver: "Mokrouxton", team: "Haas", points: 23 },
   { driver: "Ade", team: "BMW", points: 21 },
   { driver: "Justk4rt", team: "Peugeot", points: 19 },
+  { driver: "LordGRIMM", team: "Williams", points: 18 },
   { driver: "saul_security", team: "Alpine", points: 18 },
+  { driver: "N1teiger", team: "Peugeot", points: 18 },
   { driver: "DeDe", team: "Mclaren", points: 17 },
   { driver: "Kurjaka", team: "Peugeot", points: 16 },
-  { driver: "LordGRIMM", team: "Williams", points: 14 },
+  { driver: "dummy", team: "Aston", points: 12 },
   { driver: "training", team: "Mercedes", points: 12 },
+  { driver: "Curren", team: "Ferrari", points: 12 },
   { driver: "Shah", team: "Vcarb", points: 11 },
-  { driver: "dummy", team: "Aston", points: 10 },
-  { driver: "Mokrouxton", team: "Haas", points: 8 },
+  { driver: "Al3x", team: "Cadillac", points: 10 },
+  { driver: "Cavalry", team: "Mclaren", points: 8 },
   { driver: "dudegameo", team: "Alpine", points: 6 },
+  { driver: "soso", team: "Mclaren", points: 4 },
+  { driver: "f1 phantom", team: "Mercedes", points: 3 },
   { driver: "Gabe", team: "Alpine", points: 2 },
   { driver: "Leighton", team: "Red Bull", points: 2 },
   { driver: "immortal", team: "Red Bull", points: 2 },
   { driver: "imnotbern", team: "Audi", points: 1 },
   { driver: "oibossbs", team: "BMW", points: 1 },
   { driver: "Jayethanli", team: "Cadillac", points: 1 },
-  { driver: "soso", team: "Mclaren", points: 4 },
-  { driver: "f1 phantom", team: "Mercedes", points: 3 }
+  { driver: "Yanfrito", team: "Peugeot", points: 1 }
 ];
 
 const teamColors = {
@@ -38,7 +43,7 @@ const teamColors = {
   Mercedes: "95, 158, 169",
   Ferrari: "176, 69, 69",
   Alten: "98, 63, 99",
-  Cadillac: "22, 24, 34"
+  Cadillac: "102, 102, 102"
 };
 
 const teamsList = [
@@ -68,14 +73,14 @@ const teamMarks = {
   Alpine: "AP",
   Peugeot: "PG",
   Mclaren: "MC",
-  BMW: "BM",
+  BMW: "BMW",
   Mercedes: "ME",
   Williams: "WI",
   Vcarb: "VC",
   Aston: "AM",
   Haas: "HS",
   "Red Bull": "RB",
-  Cadillac: "CD",
+  Cadillac: "CAD",
   Ferrari: "FR",
   Alten: "AL"
 };
@@ -85,38 +90,36 @@ const teamLogoUrls = {
   Alpine: "https://commons.wikimedia.org/wiki/Special:Redirect/file/Alpine%20F1%20Team%20Logo.svg",
   Peugeot: "https://commons.wikimedia.org/wiki/Special:Redirect/file/Peugeot%20logo.svg",
   Mclaren: "https://commons.wikimedia.org/wiki/Special:Redirect/file/McLaren%202018%20logo.svg",
-  BMW: "https://commons.wikimedia.org/wiki/Special:Redirect/file/BMW.svg",
   Mercedes: "https://commons.wikimedia.org/wiki/Special:Redirect/file/Mercedes-Logo.svg",
   Williams: "https://commons.wikimedia.org/wiki/Special:Redirect/file/Williams%20Racing%202022%20logo.svg",
   Vcarb: "https://commons.wikimedia.org/wiki/Special:Redirect/file/Red%20Bull%20Racing%20-%202021%20Logo.svg",
   Aston: "https://commons.wikimedia.org/wiki/Special:Redirect/file/Aston%20Martin%20wordmark.svg",
   Haas: "https://commons.wikimedia.org/wiki/Special:Redirect/file/Haas%20F1%20Team%20Logo.svg",
   "Red Bull": "https://commons.wikimedia.org/wiki/Special:Redirect/file/Red%20Bull%20Racing%20-%202021%20Logo.svg",
-  Cadillac: "https://commons.wikimedia.org/wiki/Special:Redirect/file/Cadillac%20Logo%202021.svg",
   Ferrari: "https://commons.wikimedia.org/wiki/Special:Redirect/file/Ferrari%20wordmark.svg"
 };
 
 const siteMeta = {
-  lastUpdated: "07 May 2026"
+  lastUpdated: "13 May 2026"
 };
 
 const upcomingRace = {
-  title: "German GP",
-  track: "Hockenheim",
-  round: "S1 Round 4",
-  date: "10th May",
-  conditions: "32 Laps • Dry Weather",
-  startTimeIso: "2026-05-10T19:30:00+05:30"
+  title: "British GP",
+  track: "Silverstone Circuit",
+  round: "S1 Round 5",
+  date: "17th May",
+  conditions: "19:30 IST",
+  startTimeIso: "2026-05-17T19:30:00+05:30"
 };
 
 const raceResults = [
   {
     round: "Round 1",
     track: "Australian GP",
-    winner: "Iate",
+    winner: "late",
     team: "Alpine",
     results: [
-      { position: 1, driver: "Iate", racePoints: 25 },
+      { position: 1, driver: "late", racePoints: 25 },
       { position: 2, driver: "Vini", racePoints: 18 },
       { position: 3, driver: "Ade", racePoints: 15 },
       { position: 4, driver: "Yatneil", racePoints: 12 },
@@ -136,7 +139,7 @@ const raceResults = [
     results: [
       { position: 1, driver: "xyro", racePoints: 25 },
       { position: 2, driver: "Yatneil", racePoints: 18 },
-      { position: 3, driver: "Iate", racePoints: 15 },
+      { position: 3, driver: "late", racePoints: 15 },
       { position: 4, driver: "DeDe", racePoints: 12 },
       { position: 5, driver: "Kurjaka", racePoints: 10 },
       { position: 6, driver: "Vini", racePoints: 8 },
@@ -166,20 +169,20 @@ const raceResults = [
   },
   {
     round: "Round 4",
-    track: "Sprint Race",
-    winner: "xyro",
-    team: "Audi",
+    track: "German GP",
+    winner: "late",
+    team: "Alpine",
     results: [
-      { position: 1, driver: "xyro", racePoints: 8 },
-      { position: 2, driver: "Iate", racePoints: 7 },
-      { position: 3, driver: "Kurjaka", racePoints: 6 },
-      { position: 4, driver: "DeDe", racePoints: 5 },
-      { position: 5, driver: "Justk4rt", racePoints: 4 },
-      { position: 6, driver: "f1 phantom", racePoints: 3 },
-      { position: 7, driver: "LordGRIMM", racePoints: 2 },
-      { position: 8, driver: "oibossbs", racePoints: 1 },
-      { position: 9, driver: "LG501" },
-      { position: 10, driver: "training" }
+      { position: 1, driver: "late", racePoints: 25 },
+      { position: 2, driver: "N1teiger", racePoints: 18 },
+      { position: 3, driver: "Mokrouxton", racePoints: 15 },
+      { position: 4, driver: "Curren", racePoints: 12 },
+      { position: 5, driver: "Al3x", racePoints: 10 },
+      { position: 6, driver: "Cavalry", racePoints: 8 },
+      { position: 7, driver: "Yatneil", racePoints: 6 },
+      { position: 8, driver: "LordGRIMM", racePoints: 4 },
+      { position: 9, driver: "dummy", racePoints: 2 },
+      { position: 10, driver: "Yanfrito", racePoints: 1 }
     ]
   }
 ];
